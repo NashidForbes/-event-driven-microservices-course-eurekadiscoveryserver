db475cedadffdb77db05e0c5ae758498aa18b0c6
NashidForbes <NashidForbes@biohumans.com>
2023-09-24 19:01:54 -0400

    These commits fall under the feat/security branch changes for securing the spring cloud security config server and the Eureka server.  However these changes are not optimal as in particular the changes for Eureka server and Spring Cloud config server use authentication over http.
    And the Eureka server client i.e foodordering application is sending username and password over the url.

--- src/main/java/com/appsdeveloperblog/estore/discoveryserver/config/rest/RestTemplate.java
+++ src/main/java/com/appsdeveloperblog/estore/discoveryserver/config/rest/RestTemplate.java
@@ -1,4 +1,4 @@
-package com.appsdeveloperblog.estore.discoveryserver.rest;
+package com.appsdeveloperblog.estore.discoveryserver.config.rest;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;

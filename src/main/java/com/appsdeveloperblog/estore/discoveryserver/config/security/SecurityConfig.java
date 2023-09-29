cd0534df24dffb9ef287d636baa2060ad5f997a0
NashidForbes <NashidForbes@biohumans.com>
2023-09-28 23:42:11 -0400

    Discoveryserver fix for eureka clients not connecting, disabled cors in SecurityConfig.java

--- src/main/java/com/appsdeveloperblog/estore/discoveryserver/config/security/SecurityConfig.java
+++ src/main/java/com/appsdeveloperblog/estore/discoveryserver/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,10 +17,17 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http    .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/eureka/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .cors(AbstractHttpConfigurer::disable)
                 .httpBasic(withDefaults());
 
         return http.build();
     }
+
+/*    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/eureka/**");
+    }*/
 }
